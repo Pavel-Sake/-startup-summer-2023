1cb829fe7622423ff21953592c888d2360baa3b2
Pavel-Sake <sake211091@gmail.com>
2023-05-12 14:27:45 +0200

    feat: implement favorites page if no favorites items

--- src/pages/favorites/Favorites.tsx
+++ src/pages/favorites/Favorites.tsx
@@ -1,13 +1,23 @@
 import React from "react";
-import { fetchGetAccessKey } from "../../services/getAccessKey";
+
+import { Button } from '@mantine/core';
 
 import styles from "./styles.module.css";
+import commonStyles from "../../commonStyles/styles.module.css"
+
+import favoritesImg from "../../assets/icons/favorites.png";
 
 function Favorites() {
   
   return (
-    <div>
-      favorites page
+    <div className={commonStyles.wrapperSizeM}>
+      <div className={styles.emptyPage}>
+        <img className={styles.images} src={favoritesImg} alt="favorites img" />
+        <div className={styles.text}>Упс, здесь еще ничего нет!</div>
+        <Button variant="light" >
+          Поиск Вакансий
+        </Button>
+      </div>
     </div>
   );
 }
